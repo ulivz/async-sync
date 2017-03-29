efe6c48f4950c6f1de22b82cc390826f21ea37bb
toxichl <472590061@qq.com>
2017-03-30 01:38:24 +0800

    Simple Update

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,27 @@ const path = require('path');
 const { createTest } = require('./lib/createTest.js');
 const { handleAsync } = require('./lib/handleAsync.js');
 const { handleSync } = require('./lib/handleSync.js');
+const { del } = require('./lib/utils.js');
+const log = require('./lib/log')
 
 gulp.task('init', function (callback) {
-	createTest(
-		path.resolve(__dirname, 'async/raw.txt'),
-		path.resolve(__dirname, 'test'),
-		2000
-	)
-		.then(data => {
-			callback()
-		}, err => {
-			console.log('ERR')
-		})
-		.catch(err => {
-			console.log(err)
-		})
+
+	del(path.resolve(__dirname, 'test'), function () {
+		createTest(
+			path.resolve(__dirname, 'lib/raw.txt'),
+			path.resolve(__dirname, 'test'),
+			2000
+		)
+			.then(data => {
+				log.title(` √ `, `Successfully sreate ${cloneCount} test files`)
+				callback()
+			}, err => {
+				console.log('ERR')
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	});
 });
 
 gulp.task('async', function (callback) {
